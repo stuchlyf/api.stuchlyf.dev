1d4d3565c029a1b1e9900ed40f5fd4111faa29bb
Florian Stuchly <florian.stuchly@accenture.com>
2023-09-21 09:13:33 +0200

    Add opentestingapi editor as project

--- src/app/v1/projects/route.ts
+++ src/app/v1/projects/route.ts
@@ -1,6 +1,7 @@
 import {Project} from "@/models/project";
 import {NextRequest, NextResponse} from "next/server";
 import noisetoolImage from '@/../public/noisetool.png';
+import opentestingapiImage from '@/../public/opentestingapi.png';
 
 export async function GET(request: NextRequest) {
   const {protocol, host} = new URL(request.url);
@@ -13,6 +14,12 @@ export async function GET(request: NextRequest) {
       githubLink: 'https://github.com/stuchlyf/noisetool..git',
       link: 'https://noisetool.stuchlyf.dev',
       image: `${baseUrl}${noisetoolImage.src}`,
+    },
+    {
+      name: 'opentestingapi editor',
+      description: 'A graphical Editor for Testcases written with OpenTestingAPI. The project was part of my final examination for my apprenticeship.',
+      githubLink: 'https://github.com/opentestingapi/impl_editor.git',
+      image: `${baseUrl}${opentestingapiImage.src}`,
     }
   ] satisfies Project[];
 
